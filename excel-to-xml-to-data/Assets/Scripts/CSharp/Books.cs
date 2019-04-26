5838c4d9e8a0c1726ac53a15d5f0784cd954030f
duzhihuiakuaku <370789792@qq.com>
2019-04-26 10:30:24 +0800

    no message

---
+++ excel-to-xml-to-data/Assets/Scripts/CSharp/Books.cs
@@ -0,0 +1,20 @@
+﻿public class Books
+{
+    //id
+    public static int[] RoleId = {0,1};
+    //名称
+    public static string[] Name = {"张飞","赵云"};
+    //速度
+    public static float[] Speed = {1.08f,10.9f};
+    //可供招募
+    public static bool[] Recruit = {false,true};
+    //武将技
+    public static int[,] Skill = {{2,3,4},{2,3,4}};
+    //描述
+    public static string[,] Descript = {{"蛇矛斩","银月枪"},{"蛇矛斩","银月枪"}};
+    //暴击几率
+    public static float[,] Kill = {{0.5f,0.6f},{0.8f,0.9f}};
+    //几率必杀
+    public static bool[,] Snake = {{false,true},{false,false}};
+
+}
